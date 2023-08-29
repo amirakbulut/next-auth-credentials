4c6cb540e95253cf5733eb55f824137324bdba74
Amir Akbulut <amir@byron.nl>
2023-08-30 00:36:53 +0200

    Forgot the buttons

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -14,9 +14,24 @@ export default function Page() {
     }
   }
 
+  const fetchMe = async (edge: boolean = false) => {
+    const url = edge ? '/api/me/edge' : '/api/me';
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json()
+
+      console.log(data)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return <main>
     <span>Dashboard index</span>
     <button onClick={() => fetchUsers()} className="block border border-white p-3 rounded-md mb-3">Fetch users</button>
-    <button onClick={() => fetchUsers(true)} className="block border border-white p-3 rounded-md">Fetch users (edge)</button>
+    <button onClick={() => fetchUsers(true)} className="block border border-white p-3 rounded-md mb-3">Fetch users (edge)</button>
+    <button onClick={() => fetchMe()} className="block border border-white p-3 rounded-md mb-3">Fetch me</button>
+    <button onClick={() => fetchMe(true)} className="block border border-white p-3 rounded-md">Fetch me (edge)</button>
   </main>
 }
